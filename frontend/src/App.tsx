846f429c7968c35e5683d1bc1b61d4b1d3525765
Julian <julian.aschwanden@lernende.bbw.ch>
2024-03-13 17:55:19 +0100

    Feat: Implemented the Base Frontend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,10 @@
-import {useState} from 'react'
-import './App.css'
-
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-            <header>
-                <a href={"/aboutus"}>About Us</a>
-            </header>
-
-            <h1>Zoo App</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+            <div className={"title"}>
+                <h1>Willkommen zur Website des Zoo Zürichs</h1>
             </div>
-            <footer>
-                Click on the Vite and React logos to learn more
-            </footer>
         </>
     )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client"
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import AboutUs from "./components/AboutUs.tsx";
+import Tickets from "./components/Tickets.tsx";
+import AppBar from "./components/AppBar.tsx";
+import Footer from "./components/Footer.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+        <AppBar />
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/aboutUs" element={<AboutUs />} />
+            <Route path="/tickets" element={<Tickets />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+);
